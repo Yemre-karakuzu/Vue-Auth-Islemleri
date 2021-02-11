0be6c48e6812a6efe14ddd502b711eaeab6c4a8a
Yunus Emre Karakuzu <y.karakuzu@digitastic.app>
2021-02-11 13:23:17 +0300

    added:SignIn operation

--- src/pages/Auth/Auth.vue
+++ src/pages/Auth/Auth.vue
@@ -66,15 +66,19 @@ export default {
   },
   methods: {
     onSubmit() {
+      let authLink =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+      if (this.isUser) {
+        authLink =
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
+      }
+
       axios
-        .post(
-          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyBz30_0EXUddBsdUD_CpAlRhzRZ_ppzr1Q",
-          {
-            email: this.user.email,
-            password: this.user.password,
-            returnSecureToken: true,
-          }
-        )
+        .post(authLink + "AIzaSyBz30_0EXUddBsdUD_CpAlRhzRZ_ppzr1Q", {
+          email: this.user.email,
+          password: this.user.password,
+          returnSecureToken: true,
+        })
         .then((response) => {
           console.log(response);
         });
